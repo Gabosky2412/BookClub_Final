1a73c5082c0ddc666b41d5c020254a284bda3dd9
Andres Gaviria <128148162+Gabosky2412@users.noreply.github.com>
2023-11-09 13:43:01 -0500

    Vista de Libros registrados
    en este cambio se añadio que los libros aparezcan en la pagina main a medida que se van registrando

--- cliente/src/rutas/main.jsx
+++ cliente/src/rutas/main.jsx
@@ -1,21 +1,43 @@
 import Header from "./header";
-import portada from '../logos/ADMDT.png';
+import portada_nula from '../logos/PORTADA-NULA.png'
+import Axios from "axios";
+import { useState } from "react";
 
 export default function Main(){
+    const [librosList,setLibros] = useState([]);
+
+    const getUsuarios = () => {
+
+        Axios.get("http://localhost:3001/libros").then((response)=>{
+            setLibros(response.data);
+        });
+    }
+
+    getUsuarios();
+
     return(
         <div>
         <Header />
         <h1 className="font-bold text-5xl text-center mt-8 mb-14">LEE LOS MEJORES LIBROS</h1>
-            <div className="mb-12 ">
-                <div className="flex justify-center items-center border-4 border-slate-900 rounded-lg w-3/5 mx-auto shadow-2xl">
-                    <img src={portada} alt='logo_principal' className="h-80 m-8 rounded-lg border-2 border-slate-900 shadow-2xl" />
-                <div className="m-16">
-                    <h2 className="text-2xl mb-8"><strong>Título:</strong> A dos metros de ti</h2>
-                    <h2 className="text-2xl mb-8"><strong>Autor:</strong> Justin Baldoni y Cathy Schulman</h2>
-                    <h2 className="text-2xl"><strong>Género:</strong> Romance</h2>
-                </div>
-                </div>
-            </div>
+
+            {
+                librosList.map((val)=>{ 
+
+                    return <div key={val.id}>
+                                <div className="mb-12 ">
+                                    <div className="flex justify-start items-center border-4 border-slate-900 rounded-lg w-3/5 mx-auto shadow-2xl">
+                                        <img src={portada_nula} alt='logo_principal' className="h-80 m-8 rounded-lg border-2 border-slate-900 shadow-2xl" />
+                                        <div className="m-16">
+                                            <h2 className="text-2xl mb-8"><strong>Título:</strong> {val.titulo}</h2>
+                                            <h2 className="text-2xl mb-8"><strong>Autor:</strong> {val.autor}</h2>
+                                            <h2 className="text-2xl"><strong>Género:</strong> {val.genero}</h2>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                })
+            }
+            
         </div>
 
     );
